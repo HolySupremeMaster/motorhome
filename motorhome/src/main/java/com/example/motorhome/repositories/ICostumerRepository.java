a2de274b19e07ec98d870cccb7d62c85d14c40cb
omar0190 <61865216+omar0190@users.noreply.github.com>
2020-05-21 14:34:01 +0200

    omaratik

--- motorhome/src/main/java/com/example/motorhome/repositories/ICostumerRepository.java
+++ motorhome/src/main/java/com/example/motorhome/repositories/ICostumerRepository.java
@@ -1,5 +1,13 @@
 package com.example.motorhome.repositories;
 
+import com.example.motorhome.models.Customer;
+
 public interface ICostumerRepository {
+
+    public void createCustomer(Customer customer);
+
+    public boolean deleteCustomer(Customer customer);
+
+    public boolean updateCustomer(Customer customer);
 
 }
