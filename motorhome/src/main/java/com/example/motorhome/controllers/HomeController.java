dd911cb3b412db092e9ae7864847332f38bfd335
omar0190 <61865216+omar0190@users.noreply.github.com>
2020-05-22 16:03:15 +0200

    omaratik

--- motorhome/src/main/java/com/example/motorhome/controllers/HomeController.java
+++ motorhome/src/main/java/com/example/motorhome/controllers/HomeController.java
@@ -1,4 +1,33 @@
 package com.example.motorhome.controllers;
 
+import com.example.motorhome.models.Booking;
+import com.example.motorhome.repositories.BookingRepositoryDB;
+import com.example.motorhome.repositories.IBookingRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class HomeController {
+
+
+    IBookingRepository iBookingRepository;
+
+    public HomeController() {
+
+        iBookingRepository = new BookingRepositoryDB();
+
+    }
+
+    @GetMapping("/createbooking")
+    public String createBooking(){
+
+        return "booking/create";
+    }
+    @PostMapping("/created")
+    public String created(@ModelAttribute Booking booking){
+    iBookingRepository.createBooking(booking);
+    return "redirect:/createbooking";
+    }
+
 }
