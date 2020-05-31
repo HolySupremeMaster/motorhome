55a8a1f5fdee71910f8ed38eb2ab06401e3966ea
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-05-31 12:57:13 +0200

    Update MotorhomeNordicApplicationTests.java

--- motorhome-nordic/src/test/java/com/example/motorhomenordic/MotorhomeNordicApplicationTests.java
+++ motorhome-nordic/src/test/java/com/example/motorhomenordic/MotorhomeNordicApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.motorhomenordic;
 
+import com.example.motorhomenordic.repositories.BookingRepositoryDB;
+import com.example.motorhomenordic.repositories.IBookingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MotorhomeNordicApplicationTests {
