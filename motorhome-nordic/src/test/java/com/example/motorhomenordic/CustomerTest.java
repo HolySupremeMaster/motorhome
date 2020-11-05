4b0ad1693b5539fa76f0335ec5ea4cb493139975
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-11-05 19:01:19 +0100

    Update CustomerTest.java

--- motorhome-nordic/src/test/java/com/example/motorhomenordic/CustomerTest.java
+++ motorhome-nordic/src/test/java/com/example/motorhomenordic/CustomerTest.java
@@ -1,4 +1,29 @@
 package com.example.motorhomenordic;
 
+import com.example.motorhomenordic.models.Customer;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerTest {
+
+
+   @Test
+   public void getCustomer(){
+
+       //Act
+       ArrayList<Customer> list = new ArrayList<>();
+       Customer customer = new Customer("Ali", "Raza", "Ali@Hotmail.com", 23434343, 1, 21, 1);
+       list.add(customer);
+       //Lægger penge på kontoen
+       String expected = "Ali";
+       String actual = list.get(0).getFirstName();
+       //Assert
+       assertEquals(expected, actual);
+
+
+   }
 }
